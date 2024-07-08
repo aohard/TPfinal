ac5c218e53491937f6d4afb54f428cd5c5353d50
aohard <cruzjorge88@gmail.com>
2024-07-07 23:02:19 -0300

    final

--- backend/app.py
+++ backend/app.py
@@ -50,5 +50,4 @@ def delete_user(id):
         return jsonify({'message': 'Failed to delete user', 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
