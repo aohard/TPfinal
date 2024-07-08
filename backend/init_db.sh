c97214cc4ddb8eaa0a1b2bf743819b4872571a5b
aohard <cruzjorge88@gmail.com>
2024-07-07 21:30:15 -0300

    final

--- backend/init_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-service mysql start
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS dbname;"
-mysql -u root -e "CREATE USER 'user'@'%' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON dbname.* TO 'user'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
